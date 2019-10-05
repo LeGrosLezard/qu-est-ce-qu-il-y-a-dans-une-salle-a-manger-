f6b93c4829d2bbd4cbdb261652f5971cad524bbc
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-10-05 22:10:23 +0200

    Add files via upload
    et comment on fait pour la couleur ? je sais pas s'il fallait faire ce filtre ou juste e ngris

---
+++ main.py
@@ -0,0 +1,96 @@
+import os
+import cv2
+import numpy as np
+import csv
+
+liste = os.listdir(r"C:\Users\jeanbaptiste\Desktop\assiette\image\assiette")
+path = "image/assiette/{}"
+
+model = r"C:\darknet\yolov3.weigths"
+cfg = r"C:\darknet\yolov3.cfg"
+net = cv2.dnn.readNet("yolov3.weigths", "yolov3.cfg")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
