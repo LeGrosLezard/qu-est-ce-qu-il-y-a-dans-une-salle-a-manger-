3e158fae929a07e68005363974d122bb0ccba43a
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-10-06 00:57:24 +0200

    Update main.py
    53 contre 1 du coup comme ca et j'ai supprimer toutes les negs qui faisait un rond...
    1er voisin contre hyperplan (jle dis mais je sais pas ce que ca veut dire ^^)

--- main.py
+++ main.py
@@ -4,9 +4,19 @@ from sklearn.neighbors import KNeighborsClassifier
 from skimage import exposure
 from skimage import feature
 
+import numpy as np
+
 import joblib
 import cv2
+import csv
 import os
+
+import pandas as pd
+import matplotlib.pyplot as plt  
+from sklearn import metrics
+from sklearn.decomposition import PCA
+from sklearn.utils import shuffle
+
 
 
 liste = os.listdir(r"C:\Users\jeanbaptiste\Desktop\assiette\image\assiette")
@@ -16,21 +26,40 @@ path  = r"C:\Users\jeanbaptiste\Desktop\assiette\image\assiette\{}"
 liste1 = os.listdir(r"C:\Users\jeanbaptiste\Desktop\assiette\image\N")
 path1  = r"C:\Users\jeanbaptiste\Desktop\assiette\image\N\{}"
 
+
+def csv_write():
+    with open('csv/miammiamCSV.csv', 'w') as file:
+        writer = csv.writer(file)
+        file.write("label;")
+        for i in range(0, 576):
+            file.write("histo"+str(i)+";")
+
+        file.write("\n")
+
+def write_data_into_csv(data, label):
+
+    with open('csv/miammiamCSV.csv', 'a') as file:
+        writer = csv.writer(file)
+        file.write(label+";")
+        for i in data:
+            file.write(str(i)+";")
+
+        file.write("\n")
+
+
+
 data = []
 labels = []
 
-
-for nb in range(2):
-
-    if nb == 1:
-        liste = liste1
-        path = path1
+def input_csv(liste, path, label):
 
     for image in liste:
+
         #on lit l'image, to contours uniquement
         img = cv2.imread(path.format(image))
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         edged = cv2.Canny(gray, 100, 200)
+
 
         #on récupérer les contours
         contours, _ = cv2.findContours(edged, cv2.RETR_EXTERNAL,
@@ -41,6 +70,7 @@ for nb in range(2):
         for i in contours:
             if cv2.contourArea(i) >= max_cnt:
                 max_cnt = cv2.contourArea(i)
+
 
         #on fait match le plus grand contour avec la liste puis corp gray
         for i in contours:
@@ -56,38 +86,67 @@ for nb in range(2):
                 cells_per_block=(2, 2), transform_sqrt=True, block_norm="L1",
                                  visualize=True)
 
+
         #on ajoute les info pour le model
-        data.append(H)
-        if nb == 1:
-            labels.append("negativ")
-        elif nb == 0:
-            labels.append("assiette")
+        write_data_into_csv(H, label)
 
 
         #on affiche les image HOG/gray crop
-        hogImage = exposure.rescale_intensity(hogImage, out_range=(0, 255))
-        hogImage = hogImage.astype("uint8")
-
+##        hogImage = exposure.rescale_intensity(hogImage, out_range=(0, 255))
+##        hogImage = hogImage.astype("uint8")
 ##
 ##        cv2.imshow("HOG Image", hogImage)
 ##        cv2.imshow("dzq", crop)
 ##        cv2.waitKey(0)
 
 
-print(labels)
 
 
-model = svm.SVC()
-model.fit(data, labels)
+csv_write()
+input_csv(liste1, path1, "0")
+input_csv(liste, path, "1")
 
+
+file =  open('csv/miammiamCSV.csv', 'r')
+    
+dataframe = file.readlines()
+dataframe = dataframe[1:]
+
+data = []
+label = []
+increment = ""
+
+
+for i in dataframe:
+    liste_w = []
+    for j in i:
+        if j == "\n":
+            data.append(liste_w)
+            liste_w = []
+        else:
+            if j == " ":
+                pass
+            elif j == ";":
+                liste_w.append(float(increment))
+                increment = ""
+            else:
+                increment += str(j)
+
+
+for i in data:
+    label.append(int(i[0]))
+    del i[0]
+
+
+
+
+model = KNeighborsClassifier(n_neighbors=1)
+model.fit(data, label)
 joblib.dump(model, "model/miammiam_model") 
 
+predictions = model.predict(data)
 
-
-
-
-
-
+print("Score", metrics.accuracy_score(label, predictions))
 
 
 
