b572e3117c36db2324683824f6f4ef1e7557d652
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-10-07 17:35:47 +0200

    Update main.py
    y'a des result mais pas assez refaire le model en travaillant plus les images et negative ptretre pas mettre buffy chpas mais un autre truk et jvois aucun model dans backup c toutes les 100 et on est a peine a 15 ...

--- main.py
+++ main.py
@@ -20,42 +20,63 @@ for i in liste:
     img = cv2.imread(path.format(i))
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    edged = cv2.Canny(gray, 100, 200)
 
     for y in range(0, img.shape[0], 50):
         for x in range(0, img.shape[1], 50):
-
+            
             clone = img.copy()
-            crop = img[y:y+50, x:x+50]
-            cv2.rectangle(clone, (x, y), (x + 50, y + 50), (0, 255, 0), 2)
+            crop = edged[y:y+50, x:x+50]
 
+
+            #on récupérer les contours
+            contours, _ = cv2.findContours(crop, cv2.RETR_EXTERNAL,
+                                           cv2.CHAIN_APPROX_SIMPLE)
+
+            #on recupere le plus grand
+            max_cnt = 0
+            for i in contours:
+                if cv2.contourArea(i) >= max_cnt:
+                    max_cnt = cv2.contourArea(i)
+
+
+            #on fait match le plus grand contour avec la liste puis corp gray
+            for i in contours:
+                if cv2.contourArea(i) != max_cnt:
+                    cv2.drawContours(crop, i, -1, (0))
+                     
+                
             try:
 
                 (H, hogImage) = feature.hog(crop, orientations=9, pixels_per_cell=(10, 10),
                         cells_per_block=(2, 2), transform_sqrt=True, block_norm="L1", visualize=True)
 
 
-
                 hogImage = exposure.rescale_intensity(hogImage, out_range=(0, 255))
                 hogImage = hogImage.astype("uint8")
 
                 pred = model.predict(H.reshape(1, -1))[0]
-                print(pred)
+
+                if pred == 1:
+                    cv2.rectangle(img, (x, y), (x + 50, y + 50), (0, 0, 255), 2)
 
                 
             except:
                 pass
 
-            
-            #cv2.circle(clone, (int(x+50/2), int(y+50/2)), 3, (0,0,255), 3)
+
+                     
+            cv2.rectangle(clone, (x, y), (x + 50, y + 50), (0, 255, 0), 2)
 
 
             cv2.imshow("Window", clone)
-            cv2.imshow("img", hogImage)
+            cv2.imshow("img", crop)
             cv2.waitKey(1)
             time.sleep(0.5)
 
 
-
+    cv2.imshow("imgage", img)
+    cv2.waitKey(0)
 
 
 
