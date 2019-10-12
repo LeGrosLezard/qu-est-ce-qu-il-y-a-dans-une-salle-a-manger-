7cce412e2f1081d335f242be086f09a28f3fedcd
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-10-12 16:16:41 +0200

    Update detection.py

--- detection.py
+++ detection.py
@@ -11,7 +11,7 @@ import joblib
 import imutils
 
 
-#---------------------------------- picture treatment
+#--------------------------------------------------------------------------------- picture treatment
 def open_picture(image):
 
     """We open picture"""
@@ -20,13 +20,31 @@ def open_picture(image):
     return img
 
 
+"""
+    Resumé:
+    En gros ici on a :
+    A améliorer:
+"""
+
+
+
+
+#
 def show_picture(name, image, mode, destroy):
     cv2.imshow(name, image)
     cv2.waitKey(mode)
     if destroy == "y":
         cv2.destroyAllWindows()
 
+"""
+    Resumé:
+    En gros ici on a :
+    A améliorer:
+"""
 
+
+
+#
 def blanck_picture(img):
 
     """Create a black background picture same dimension of original picture"""
@@ -36,20 +54,109 @@ def blanck_picture(img):
 
     return blank_image
 
-#---------------------------------- picture treatment
+
+
+"""
+    Resumé:
+    En gros ici on a :
+    A améliorer:
+"""
 
 
 
 
 
 
+
+
+
+
+
+#--------------------------------------------------------------------------------- picture croping
+
+#
+def get_other_object(img):
+
+    """We detecte contours from the picture
+    We try to recup them and to put it into rectangle"""
+
+    blanck = blanck_picture(img)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    edged = cv2.Canny(img, 0, 160)
+
+    contours, _ = cv2.findContours(edged, cv2.RETR_TREE,
+                                   cv2.CHAIN_APPROX_SIMPLE)
+
+    liste = []
+    for cnts in contours:
+        x, y, w, h = cv2.boundingRect(cnts)
+        liste.append([x, y, w, h])
+        cv2.drawContours(blanck, cnts, -1, (255, 255, 255), 1)
+
+    show_picture("blanck", blanck, 0, "y")
+    return liste
+
+"""
+    Resumé:
+    En gros ici on a :
+    A améliorer:
+"""
+
+
+
+
+#
+def croping_it_from_original(img, liste):
+
+    """Now we must fusion the multiples detections from
+    a single object.
+    We must crop it now for an other matching with current model"""
+
+    liste_area = []
+    for i in liste:
+        if i[2] > 10 and i[3] > 10:
+            liste_area += [i]
+
+    for i in liste_area:
+        cv2.rectangle(img, (i[0], i[1]), (i[0] + i[2], i[1] + i[3]), (0, 0, 255), 1)
+
+    show_picture("img", img, 0, "")
+
+
+"""
+    Resumé:
+    En gros ici on a :
+    A améliorer:
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#--------------------------------------------------------------------------------- picture detection
+
+#
 def HOG_detection(gray):
 
     """We detect contour orientation gradient
     from color"""
 
-    (H, hogImage) = feature.hog(gray, orientations=9, pixels_per_cell=(10, 10),
-                                cells_per_block=(2, 2), transform_sqrt=True,
+    (H, hogImage) = feature.hog(gray, orientations=9,
+                                pixels_per_cell=(10, 10),
+                                cells_per_block=(2, 2),
+                                transform_sqrt=True,
                                 block_norm="L1", visualize=True)
 
     hogImage = exposure.rescale_intensity(hogImage, out_range=(0, 255))
@@ -58,9 +165,17 @@ def HOG_detection(gray):
     return H, hogImage
 
 
+"""
+    Resumé:
+    En gros ici on a :
+    A améliorer:
+"""
 
 
 
+
+
+#
 def reverse_img_by_pos(img, x, y, size):
 
     """ 
@@ -72,10 +187,8 @@ def reverse_img_by_pos(img, x, y, size):
 
     
     height, width, channel = img.shape
+    reverse_x = False; reverse_y = False
 
-    reverse_x = False
-    reverse_y = False
-    
     if x >= width - size:
         reverse_x = True
 
@@ -94,18 +207,41 @@ def reverse_img_by_pos(img, x, y, size):
         crop = img[y:y+size*5, x:x+size*5]
         pts = (y, y+size*5, x, x+size*5)
 
-
     return crop, pts
 
 
+"""
+    Resumé:
+    En gros ici on a :
+    A améliorer:
+"""
 
+
+
+
+
+#
+def rotation_crop(crop):
+
+    for i in range(0, 360, 10):
+        rotated = imutils.rotate_bound(crop, i)
+
+"""
+    Resumé:
+    En gros ici on a :
+    A améliorer:
+"""
+
+
+
+
+#
 def parcours_image(img, model):
 
     """We put picture into model. We try to match
     The last crop with picture on model"""
 
-    size = 25
-    list_intersection = []
+    size = 25; list_intersection = []
     img_detection = img.copy()
 
     for y in range(0, img.shape[0], size):
@@ -115,44 +251,51 @@ def parcours_image(img, model):
 
             try:
                 crop, pts = reverse_img_by_pos(img, x, y, size)
-
                 crop = cv2.resize(crop, (50, 50))
                 gray = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
-
                 H, hogImage = HOG_detection(gray)
+                rotation_crop(hogImage)
                 pred = model.predict(H.reshape(1, -1))[0]
-
                 if pred == 1:
-                    cv2.rectangle(img_detection, (pts[2], pts[0]), (pts[3], pts[1]), (0, 0, 255), 2)
-                    list_intersection.append([pts[2], pts[0], pts[3], pts[1]])
+                    cv2.rectangle(img_detection, (pts[2], pts[0]),
+                                  (pts[3], pts[1]), (0, 0, 255), 2)
+
+                    list_intersection.append([pts[2], pts[0],
+                                              pts[3], pts[1]])
             except:
                 pass
 
 
-##            cv2.rectangle(clone, (x, y), (x + size, y + size), (0, 255, 0), 2)
-##            show_picture("clone", clone, 1, "")
-##            show_picture("img_detection", img_detection, 1, "")
-##            time.sleep(0.3)
+            cv2.rectangle(clone, (x, y), (x + size, y + size), (0, 255, 0), 2)
+            show_picture("clone", clone, 1, "")
+            show_picture("img_detection", img_detection, 1, "")
+            time.sleep(0.3)
 
     return list_intersection
 
 
+"""
+    Resumé:
+    En gros ici on a :
+    A améliorer:
+"""
+
+
+
+
+
+#
 def reconstruction(image, liste):
 
     """We can have multiple detection so
     We make an average of this and recup the final detection
     We return this points"""
 
-    a = 0
-    b = 0
-    c = 0
-    d = 0
+    a = 0; b = 0; c = 0; d = 0
 
     for i in liste:
-        a += i[0]
-        b += i[1]
-        c += i[2]
-        d += i[3]
+        a += i[0]; b += i[1]
+        c += i[2]; d += i[3]
 
     e = len(liste)
 
@@ -163,52 +306,25 @@ def reconstruction(image, liste):
 
 
 
-def get_other_object(img):
-
-    """We detecte contours from the picture
-    We try to recup them and to put it into rectangle"""
-
-    blanck = blanck_picture(img)
-
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    th3 = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-                                cv2.THRESH_BINARY,11,10)
-
-    contours, _ = cv2.findContours(th3, cv2.RETR_TREE,
-                                   cv2.CHAIN_APPROX_SIMPLE)
-
-    liste = []
-    for cnts in contours:
-        if 30000 > cv2.contourArea(cnts) > 200:
-            x, y, w, h = cv2.boundingRect(cnts)
-            liste.append([x, y, w, h])
-            cv2.drawContours(blanck, cnts, -1, (255, 255, 255), 1)
-
-
-    show_picture("blanck", blanck, 0, "y")
-    return liste
+"""
+    Resumé:
+    En gros ici on a :
+    A améliorer:
+"""
 
 
 
 
-def croping_it_from_original(img, liste, x, y, w, h):
-
-    """Now we must fusion the multiples detections from
-    a single object.
-    We must crop it now for an other matching with current model"""
-
-    print(liste)
-
-    for i in liste:
-        print(i)
-        cv2.rectangle(img, (i[0], i[1]), (i[0] + i[2], i[1] + i[3]), (0, 0, 255), 1)
-
-    show_picture("img", img, 0, "")
 
 
 
 
+
+
+
+
+
+#--------------------------------------------------------------------------------- MAIN
 def detection_picture():
 
     #load model
@@ -218,20 +334,21 @@ def detection_picture():
     img = open_picture("dataset1/image/assiette_couvert/assiette1.jpg")
     img_copy = img.copy()
 
+
+    #get other items on picture
+    liste = get_other_object(img)
+
+    """     IN COURSE    """    
+    croping_it_from_original(img_copy, liste)
+
+
+
     #recup all detection
     list_intersection = parcours_image(img, model)
 
     #take intersection detection
     x, y, w, h = reconstruction(img, list_intersection)
 
-    #get other items on picture
-    liste = get_other_object(img)
-
-    """     IN COURSE    """    
-    croping_it_from_original(img_copy, liste, x, y, w, h)
 
 
-
-#on doit récupérer les parties de l'images ou du moin
-#match toutes les parties
 detection_picture()
