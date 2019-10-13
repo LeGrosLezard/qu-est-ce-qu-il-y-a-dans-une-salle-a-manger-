f73995e4c6e8d4cf22d38a39021aeced917696af
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-10-13 03:39:44 +0200

    Update main.py

--- program/main.py
+++ program/main.py
@@ -1,3 +1,20 @@
+from label.search_label import element_in_label_PY
+
+from detection.crop_objects import detection_picture
+from detection.detection import detection_picture_hog
+
+
+
+
+def detection_object():
+
+    """
+        We recup the label from label.py
+        we try to detect one of label from model
+    """
+
+    number_label, label = element_in_label_PY()
+    detection_picture_hog(number_label, label)
 
 
 
@@ -8,17 +25,9 @@
 
 
 
-
-
-
-
-
-
-
-    
 
 if __name__ == "__main__":
-    pass
+    detection_object()
 
 
 
