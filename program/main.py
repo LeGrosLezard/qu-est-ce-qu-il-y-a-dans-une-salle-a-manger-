4f62afc8f3f18765ff44c9f28722ceaa55cae09f
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-10-13 22:12:52 +0200

    Update main.py
    c'est encore un pele mele faut simplifier

--- program/main.py
+++ program/main.py
@@ -1,5 +1,5 @@
 import os
-
+import cv2
 
 from label.search_label import element_in_label_PY
 def our_object():
@@ -53,10 +53,54 @@ def detection_object(model, image, number_label, label,
 
 
 
-def chercher_objet(objects, objects_detected):
-    pass
+from searching_data.object_category import our_dico_path_url
+from searching_data.object_category import searching_category
+from searching_data.object_category import other_element_from_category
+from searching_data.object_category import transform_category_to_object
+
+def search_objet(objects, objects_detected, OBJECT_SEARCHING):
 
 
+    our_path = our_dico_path_url()
+
+
+    for i in range(len(objects)):
+        if objects_detected[i] is None:
+            OBJECT_SEARCHING.append(objects[i])
+            print(objects[i], "no found")
+
+
+    for i in range(len(objects)):
+        if objects_detected[i] is not None:
+
+            print(objects[i], "Found\n")
+            category = searching_category(objects_detected[i], our_path)
+
+            category_found = other_element_from_category(category,
+                                                         objects_detected[i],
+                                                         our_path)
+
+            objects_to_search = transform_category_to_object(category_found, our_path)
+
+    
+    return objects_to_search 
+    
+
+from download_data.download_data import transform_i
+from download_data.download_data import main_download
+def download_picture(objects_to_search):
+
+    for objects in objects_to_search:
+        objects = transform_i(objects)
+        main_download([objects], [200], [0], "dataset/")
+
+
+from clean_data.main import take_features
+def cleanning_dataset(objects_to_search):
+
+    #['Couteau', 'Cuillère', 'Fourchette']
+    take_features(objects_to_search)
+            
 
 
 if __name__ == "__main__":
@@ -65,24 +109,44 @@ if __name__ == "__main__":
     image = "dataset/assiette_couvert/assiette1.jpg"
     path_to_analysis = "dataset/data_analysing/"
 
+    OBJECT_SEARCHING = []
 
     #1 on coupe tout -> detecter par exemple un manche
     #2 on fait une detection pour voir si on a l'objet
     #3 si on a objet afficher
     #4 si on a pas objet: rechercher
 
-
+    """label.search_label.py"""
     number_label, label = our_object()
 
+    """detection.crop_objects"""
     #detect_objects(model, image)
 
+    """detection.detection"""
+    #objects, objects_detected\
+    #= detection_object(model, image, number_label, label,
+    #                   path_to_analysis)
 
-    objects, objects_detected\
-    = detection_object(model, image,
-                       number_label, label,
-                       path_to_analysis)
+    """searching_data.object_category"""
+    #objects_to_search = search_objet(objects, objects_detected, OBJECT_SEARCHING)
 
-    chercher_objet(objects, objects_detected)
+
+    """download_data.download_data"""
+    #download_picture(objects_to_search)
+
+
+    """clean_data.main"""
+    objects_to_search = ['Couteau', 'Cuillère', 'Fourchette']
+    cleanning_dataset(objects_to_search)
+
+
+
+
+
+
+
+
+
 
 
 
