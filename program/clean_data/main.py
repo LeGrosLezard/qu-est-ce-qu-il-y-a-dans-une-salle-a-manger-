c980a53b3242d2bf1175d2992780df77d7a23661
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-10-13 22:13:10 +0200

    Update main.py

--- program/clean_data/main.py
+++ program/clean_data/main.py
@@ -6,43 +6,38 @@ import math
 from PIL import Image
 
 
-def take_features(liste_obj, category):
+def open_picture(image):
 
-    path = "dataset1/{}/{}"
-    path_clean = "dataset1/clean/{}/{}"
-    for i in liste_obj:
+    """We open picture"""
 
-        print("picture: ", i, "\n")
-        img = open_picture(path.format(category, i))
-        
-
-        height, width, channel = img.shape
-        if height > 200 and width > 200:
-            img = cv2.resize(img, (200, 200))
-
-        blanck = blanck_picture(img)
-
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        edged = cv2.Canny(img, 100, 200)
-
-        contours, _ = cv2.findContours(edged, cv2.RETR_TREE,
-                                       cv2.CHAIN_APPROX_SIMPLE)
-
-        treatment_background(img, path_clean, category)  #background
-        multiple_objects(contours, blanck, img, path_clean, category)
-        position_rotation(contours, blanck, img, path_clean, category) #rotation
-        
-        break
-
-def open_download_folder(objects_to_search):
-
-    for i in objects_to_search:
-
-        liste_obj = os.listdir("dataset1/" + i)
-        contours_obj, blanck = take_features(liste_obj, i)  #take_features
+    img = cv2.imread(image)
+    return img
 
 
-def treatment_background(img, path_clean, category):
+def show_picture(name, image, mode, destroy):
+    cv2.imshow(name, image)
+    cv2.waitKey(mode)
+    if destroy == "y":
+        cv2.destroyAllWindows()
+
+
+
+def blanck_picture(img):
+
+    """Create a black background picture same dimension of original picture"""
+
+    blank_image = np.zeros((img.shape[0],img.shape[1],3), np.uint8)
+    blank_image[0:img.shape[0], 0:img.shape[1]] = 0, 0, 0
+
+    return blank_image
+
+
+from clean_data.background import main_color_background
+from clean_data.background import make_cnts
+from clean_data.background import find_contour_else_white
+from clean_data.background import finish_to_clean_background
+from clean_data.background import masking_to_black
+def treatment_background(img, category):
     name = str(img)
     img = cv2.resize(img, (200, 200))
     color = main_color_background(img)
@@ -61,6 +56,12 @@ def treatment_background(img, path_clean, category):
 
         #it'so ok now
         img = masking_to_black(img, color)
+
+    else:
+        pass
+
+    return img
+
 
 
 
@@ -82,10 +83,48 @@ def position_rotation(contours, blanck, img, path_clean, category):
                   X_min2, Xy_min2, X_max2, Xy_max2,
                   position, blanck, path_clean, category)
 
-    
-
-if __name__ == "__main__":
 
 
-    objects_to_search = ["cuillere", "fourchette", "couteau", "verre"]
-    open_download_folder(objects_to_search)
+def take_features(objects_to_search):
+
+
+    path_folder = "dataset/{}"
+    path_picture = "dataset/{}/{}"
+    path_clean = "dataset/clean/{}/{}"
+
+
+    for objects in objects_to_search:
+        os.makedirs(path_picture.format("clean", objects))
+        liste_obj = os.listdir(path_folder.format(objects))
+        for image in liste_obj:
+
+            print("picture: ", image, "\n")
+
+            img = open_picture(path_picture.format(objects, image))
+
+            height, width, channel = img.shape
+            if height > 200 and width > 200:
+                img = cv2.resize(img, (200, 200))
+
+            blanck = blanck_picture(img)
+
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            edged = cv2.Canny(img, 100, 200)
+
+            contours, _ = cv2.findContours(edged, cv2.RETR_TREE,
+                                           cv2.CHAIN_APPROX_SIMPLE)
+
+            img = treatment_background(img, objects)  #background
+            print(path_clean.format(objects, image))
+            cv2.imwrite(path_clean.format(objects, image), img)
+
+
+##            multiple_objects(contours, blanck, img, path_clean, category)
+##            position_rotation(contours, blanck, img, path_clean, category) #rotation
+##            
+##            break
+
+
+
+
+
