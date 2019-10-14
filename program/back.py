bc1226c9b677ce0b714d20c6ac6422b56bb96a59
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-10-14 22:19:37 +0200

    Update back.py
    faut remplacer background par back mtn mais avant rotation

--- program/back.py
+++ program/back.py
@@ -86,7 +86,7 @@ def contours_square(img, name):
             M = cv2.moments(cnt)
             cX = int(M["m10"] / M["m00"])
             cY = int(M["m01"] / M["m00"])
-            cv2.drawContours(img,[cnt],-1,(0,255,0),1)
+            #cv2.drawContours(img,[cnt],-1,(0,255,0),1)
         except:
             pass
 
@@ -131,66 +131,148 @@ def contours_square(img, name):
         img = crop
 
 
+    else:
+        color = main_color_background(img)
+        print(color)
+        show_picture("img", img, 0, "y")
+
+        if color != (255, 255, 255):
+
+            th3 = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+                                        cv2.THRESH_BINARY,11,10)
+
+            show_picture("th3", th3, 0, "y")
 
 
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    _,thresh = cv2.threshold(gray,250,255,cv2.THRESH_BINARY_INV)
+            blanck0 = blanck_picture(img)
+            copy5 = img.copy()
 
-    show_picture("thresh", thresh, 0, "y")
+     
+            cont,h=cv2.findContours(th3,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+            for cn in cont:
+                if cv2.contourArea(cn) < 10000:
+                    cv2.drawContours(copy5,[cn],-1,(0,255,0),3)
+                    cv2.fillPoly(copy5, pts =[cn], color=(0,255,0))
+
+            show_picture("copy5", copy5, 0, "y")
 
 
-    contours,h=cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-
-    blanck1 = blanck_picture(img)
-
-    print(len(contours))
-
-    c = 0
-    for cnt in contours:
-        if cv2.contourArea(cnt) > 10:
-
-            blanck1 = blanck_picture(img)
-            
-            copy = img.copy()
-
-            cv2.drawContours(blanck1,[cnt],-1,(0,255,0),1)
-            cv2.fillPoly(blanck1, pts =[cnt], color=(0,255,0))
-
-            for i in range(blanck1.shape[0]):
-                for j in range(blanck1.shape[1]):
-                    if blanck1[i, j][0] == 0 and\
-                       blanck1[i, j][1] == 255 and\
-                       blanck1[i, j][2] == 0:
+            for i in range(copy5.shape[0]):
+                for j in range(copy5.shape[1]):
+                    if copy5[i, j][0] == 0 and\
+                       copy5[i, j][1] == 255 and\
+                       copy5[i, j][2] == 0:
                         pass
                     else:
-                        copy[i, j] = 255, 255, 255
+                        copy5[i, j] = 255, 255, 255
+                        
 
-            show_picture("blanck1", blanck1, 0, "y")
-            show_picture("copy", copy, 0, "y")
+            show_picture("copy5", copy5, 0, "y")
 
-
-            gray = cv2.cvtColor(copy, cv2.COLOR_BGR2GRAY)
+            gray = cv2.cvtColor(copy5, cv2.COLOR_BGR2GRAY)
             _,thresh = cv2.threshold(gray,250,255,cv2.THRESH_BINARY_INV)
+            cont,h=cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
             show_picture("thresh", thresh, 0, "y")
 
-
-            contours,h=cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-            for cnts in contours:
-                print(cv2.contourArea(cnts))
-                if cv2.contourArea(cnts) > 9000:
-                    #os.remove(name)
-                    pass
-
-
-
-            name = str(name) + str(c) + ".jpg"
             
-            #cv2.imwrite(name, copy)
-            c += 1
+            blanck10 = blanck_picture(img)
+            for cn in cont:
+                print(cv2.contourArea(cn))
+                if cv2.contourArea(cn) < 10000:
+                    cv2.drawContours(blanck10,[cn],-1,(255,255,255),1)
+                    cv2.fillPoly(blanck10, pts =[cn], color=(255,255,255))
+
+            show_picture("blanck10", blanck10, 0, "y")
+            
+
+            for i in range(blanck10.shape[0]):
+                for j in range(blanck10.shape[1]):
+                    if blanck10[i, j][0] == 255 and\
+                       blanck10[i, j][1] == 255 and\
+                       blanck10[i, j][2] == 255:
+                        pass
+                    else:
+                        img[i, j] = 255, 255, 255
 
 
 
+            show_picture("img", img, 0, "y")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+##    _,thresh = cv2.threshold(gray,250,255,cv2.THRESH_BINARY_INV)
+##
+##    show_picture("thresh", thresh, 0, "y")
+##
+##
+##    contours,h=cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+##
+##    blanck1 = blanck_picture(img)
+##
+##    print(len(contours))
+##
+##    c = 0
+##    for cnt in contours:
+##        if cv2.contourArea(cnt) > 10:
+##
+##            blanck1 = blanck_picture(img)
+##            
+##            copy = img.copy()
+##
+##            cv2.drawContours(blanck1,[cnt],-1,(0,255,0),1)
+##            cv2.fillPoly(blanck1, pts =[cnt], color=(0,255,0))
+##
+##            for i in range(blanck1.shape[0]):
+##                for j in range(blanck1.shape[1]):
+##                    if blanck1[i, j][0] == 0 and\
+##                       blanck1[i, j][1] == 255 and\
+##                       blanck1[i, j][2] == 0:
+##                        pass
+##                    else:
+##                        copy[i, j] = 255, 255, 255
+##
+##            show_picture("blanck1", blanck1, 0, "y")
+##            show_picture("copy", copy, 0, "y")
+##
+##
+##            gray = cv2.cvtColor(copy, cv2.COLOR_BGR2GRAY)
+##            _,thresh = cv2.threshold(gray,250,255,cv2.THRESH_BINARY_INV)
+##
+##            show_picture("thresh", thresh, 0, "y")
+##
+##
+##            contours,h=cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+##            for cnts in contours:
+##                print(cv2.contourArea(cnts))
+##                if cv2.contourArea(cnts) > 9000:
+##                    #os.remove(name)
+##                    pass
+##
+##
+##
+##            name = str(name) + str(c) + ".jpg"
+##            
+##            #cv2.imwrite(name, copy)
+##            c += 1
+##
+##
+##
 
 
 
@@ -208,7 +290,7 @@ for i in liste:
     i = str("test/") + str(i)
 
     img = open_picture(i)
-    
+    img = cv2.resize(img, (200, 200))
     color = main_color_background(img)
 
     print(color)
