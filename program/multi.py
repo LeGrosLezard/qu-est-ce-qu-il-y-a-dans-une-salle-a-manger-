5552c008ea6e958b220478112c6f0fea084b4380
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-10-15 22:16:27 +0200

    Add files via upload
    bon j'ai re faim mtn

---
+++ program/multi.py
@@ -0,0 +1,104 @@
+import numpy as np
+import cv2
+import os
+import imutils
+import math
+from PIL import Image
+
+
+
+
+
+
+def show_picture(name, image, mode, destroy):
+    cv2.imshow(name, image)
+    cv2.waitKey(mode)
+    if destroy == "y":
+        cv2.destroyAllWindows()
+
+
+
+liste = os.listdir("test/")
+for i in liste:
+    i = str("test/") + str(i)
+
+    img = open_picture(i)
+    img = cv2.resize(img, (200, 200))
+
+
+    show_picture("img", img, 0, "y")
+
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _,thresh = cv2.threshold(gray,250,255,cv2.THRESH_BINARY_INV)
+
+    show_picture("thresh", thresh, 0, "y")
+
+
+    contours,h=cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+    blanck1 = blanck_picture(img)
+
+    print(len(contours))
+
+    c = 0
+    for cnt in contours:
+        if cv2.contourArea(cnt) > 10:
+
+            blanck1 = blanck_picture(img)
+            
+            copy = img.copy()
+
+            cv2.drawContours(blanck1,[cnt],-1,(0,255,0),1)
+            cv2.fillPoly(blanck1, pts =[cnt], color=(0,255,0))
+
+            for i in range(blanck1.shape[0]):
+                for j in range(blanck1.shape[1]):
+                    if blanck1[i, j][0] == 0 and\
+                       blanck1[i, j][1] == 255 and\
+                       blanck1[i, j][2] == 0:
+                        pass
+                    else:
+                        copy[i, j] = 255, 255, 255
+
+            show_picture("blanck1", blanck1, 0, "y")
+            show_picture("copy", copy, 0, "y")
+
+
+            gray = cv2.cvtColor(copy, cv2.COLOR_BGR2GRAY)
+            _,thresh = cv2.threshold(gray,250,255,cv2.THRESH_BINARY_INV)
+
+            show_picture("thresh", thresh, 0, "y")
+
+
+            contours,h=cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+            for cnts in contours:
+                print(cv2.contourArea(cnts))
+                if cv2.contourArea(cnts) > 9000:
+                    #os.remove(name)
+                    pass
+
+
+
+            name = str(name) + str(c) + ".jpg"
+            
+            #cv2.imwrite(name, copy)
+            c += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
