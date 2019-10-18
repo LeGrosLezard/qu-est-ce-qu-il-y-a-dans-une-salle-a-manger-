cac0b6f7ae735009ddf12e4e46b51fc9496dee8a
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-10-18 03:10:41 +0200

    Update comparaison.py

--- program/detection/comparaison.py
+++ program/detection/comparaison.py
@@ -49,35 +49,25 @@ def blanck_picture(img):
 
 
 
-
-
-def main_comparaison(img):
-
-    img = open_picture(img)
+def make_contours(img, blanck):
     
-
-    img = cv2.resize(img, (50, 200))
-    copy = img.copy()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-
     _,thresh = cv2.threshold(gray,250,255,cv2.THRESH_BINARY_INV)
-
-    show_picture("thresh", thresh, 0, "y")
-
-
-    blanck = blanck_picture(img)
 
     contours,h=cv2.findContours(thresh,cv2.RETR_EXTERNAL,
                                     cv2.CHAIN_APPROX_SIMPLE)
 
-    
     for i in contours:
         if cv2.contourArea(i) > 200:
             cv2.fillPoly(blanck, pts =[i], color=(0,255,0))
 
     show_picture("blanck", blanck, 0, "y")
 
+
+    return blanck
+
+
+def object_contours_size(blanck):
 
     cnts_points = []
     for i in range(blanck.shape[0]):
@@ -93,24 +83,23 @@ def main_comparaison(img):
         else:
             cnts_points.append(c)
 
-    show_picture("blanck", blanck, 0, "y")
-
-
-
 
     mean = sum(cnts_points) / len(cnts_points)
 
     points = []
-    
     for nb, i in enumerate(cnts_points):
-
         if i > mean + 6:
             points.append(nb)
 
-    print(mean)
-    no = False
-    ok = 0
-    nan = False
+    return points
+
+
+
+
+def take_max_size(points, img):
+
+    ok = 0; nan = False;
+
     for i in range(len(points)):
         try:
             if points[i] + 1 == points[i+1] and nan is False:
@@ -122,93 +111,100 @@ def main_comparaison(img):
                               (img.shape[0], points[i]),
                               (0,0,255), 30)
                 ok = 0
-
         except:
-            no = True
+            pass
 
-    
+ 
     show_picture("img", img, 0, "y")
-
-    if no is False:
-        blanck1 = blanck_picture(img)
-        blanck2 = blanck_picture(img)
-
-
-        maxi = 0
-        c = 0
-        for i in range(img.shape[0]):
-            for j in range(img.shape[1]):
-                if img[i, j][0] == 0 and\
-                   img[i, j][1] == 0 and\
-                   img[i, j][2] == 255:
-                    blanck1[i, j] = copy[i, j]
-
-                else:
-                    blanck2[i, j] = copy[i, j]
-
-        
+    return img
 
 
 
 
-        
-        show_picture("blanck1", blanck1, 0, "y")
-        #show_picture("blanck2", blanck2, 0, "y")
+def draw_part_picture(img, copy):
+    
+    blanck1 = blanck_picture(img)
+    blanck2 = blanck_picture(img)
+
+    maxi = 0
+    c = 0
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            if img[i, j][0] == 0 and\
+               img[i, j][1] == 0 and\
+               img[i, j][2] == 255:
+                blanck1[i, j] = copy[i, j]
+
+            else:
+                blanck2[i, j] = copy[i, j]
+
+    show_picture("blanck1", blanck1, 0, "y")
+    show_picture("blanck2", blanck2, 0, "y")
+
+    return blanck1, blanck2
+
+
+def full_white(img):
+
+    full = img.shape[0] * img.shape[1]
+
+    c = 0
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            if img[i, j][0] == 0 and\
+               img[i, j][1] == 255 and\
+               img[i, j][2] == 0:
+                c+=1
+
+    print(c, full)
+    if c > 8500:
+        return False
+    else:
+        return True
+    
+
+def final_part(blanck):
+
+    gray = cv2.cvtColor(blanck, cv2.COLOR_BGR2GRAY)
+    _,thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY)
+
+    contours,h=cv2.findContours(thresh,cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)
+    maxi = 0
+    for cnt in contours:
+        if cv2.contourArea(cnt) > maxi:
+            maxi = cv2.contourArea(cnt)
+
+    for cnt in contours:
+        if cv2.contourArea(cnt) != maxi:
+            cv2.fillPoly(blanck, pts =[cnt], color=(0,0,0))
+
+    return blanck
 
 
 
-        gray = cv2.cvtColor(blanck1, cv2.COLOR_BGR2GRAY)
 
-        _,thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY)
-     
+def main_comparaison(img):
 
+    img = open_picture(img)
+    img = cv2.resize(img, (50, 200))
 
+    copy = img.copy()
 
-        contours,h=cv2.findContours(thresh,cv2.RETR_EXTERNAL,
-                                    cv2.CHAIN_APPROX_SIMPLE)
-        maxi = 0
-        for cnt in contours:
-            if cv2.contourArea(cnt) > maxi:
-                maxi = cv2.contourArea(cnt)
+    blanck = blanck_picture(img)
+    blanck = make_contours(img, blanck)
+    ocontinuer = full_white(blanck)
 
+    if ocontinuer is True:
 
-        for cnt in contours:
-            if cv2.contourArea(cnt) != maxi:
-                cv2.fillPoly(blanck1, pts =[cnt], color=(0,0,0))
-                
-        show_picture("blanck1", blanck1, 0, "y")
+        points = object_contours_size(blanck)
 
+        img = take_max_size(points, img)
 
+        blanck1, blanck2 = draw_part_picture(img, copy)
 
-        gray = cv2.cvtColor(blanck2, cv2.COLOR_BGR2GRAY)
-        show_picture("gray", gray, 0, "y")
-        _,thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY)
-        show_picture("thresh", thresh, 0, "y")
-
-
-
-        contours,h=cv2.findContours(thresh,cv2.RETR_EXTERNAL,
-                                    cv2.CHAIN_APPROX_SIMPLE)
-        maxi = 0
-        for cnt in contours:
-            if cv2.contourArea(cnt) > maxi:
-                maxi = cv2.contourArea(cnt)
-
-
-        for cnt in contours:
-            if cv2.contourArea(cnt) != maxi:
-                cv2.fillPoly(blanck2, pts =[cnt], color=(0,0,0))
-                
-        show_picture("blanck2", blanck2, 0, "y")
-
-
-
-
-
-
-
-        
-
+        blanck1 = final_part(blanck1)
+        blanck2 = final_part(blanck2)
 
 
 
