de456a27e0c4177349e2198d1311c55f9f6702b8
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-10-21 20:07:02 +0200

    Update main_function_image.py

--- v2/main_function_image.py
+++ v2/main_function_image.py
@@ -1,7 +1,7 @@
 import os
 import cv2
 import time
-
+import numpy as np
 
 def open_picture(image):
     """We open picture"""
@@ -26,3 +26,38 @@ def show_picture(name, image, mode, destroy):
 
 def save_picture(name, picture):
     cv2.imwrite(name, picture)
+
+
+def blanck_picture(img):
+    """
+        Create a black picture
+    """
+
+    blank_image = np.zeros((img.shape[0],img.shape[1],3), np.uint8)
+    blank_image[0:, 0:] = 0, 0, 0
+
+    return blank_image
+
+
+def to_crop(img):
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _,thresh = cv2.threshold(gray,250,255,cv2.THRESH_BINARY_INV)
+
+    contours,h=cv2.findContours(thresh,cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)
+
+    maxi = 0
+    for cnt in contours:
+        if cv2.contourArea(cnt) > maxi:
+            maxi = cv2.contourArea(cnt)
+
+    for cnt in contours:
+        if cv2.contourArea(cnt) == maxi:
+            maxi = cv2.contourArea(cnt)
+            x, y, w, h = cv2.boundingRect(cnt)
+
+    img = img[y:y+h, x:x+w]
+
+    return img
+
