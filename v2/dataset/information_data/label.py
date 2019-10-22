6a871a11b48b4125a0590098e9c79b252e55a67c
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-10-22 03:12:31 +0200

    Update label.py

--- v2/dataset/information_data/label.py
+++ v2/dataset/information_data/label.py
@@ -1,5 +1,5 @@
 1;assiette;1;None;50x50;
-1;chaussure;1;None;50x50;
-1;fourchette;1;manche,tine;50x50;
-1;coucou;1;chouette,coucou;50x50;
-1;czacza;1;None;50x50;
+1;chaussure;2;None;100x200;
+1;fourchette;3;manche,tine;50x50;
+1;coucou;4;chouette,coucou;50x50;
+1;czacza;5;None;50x50;
