8b7268a639bd7dd8c534d094f290a3b7a342f78d
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-10-22 16:16:49 +0200

    Update main_function_image.py

--- v2/main_function_image.py
+++ v2/main_function_image.py
@@ -59,13 +59,19 @@ def to_crop(img):
 
     img = img[y:y+h, x:x+w]
 
-    return img, int(x+w/2)
+    return img, int(x+w/2), y
 
 
-def write_position(position, name):
+def write_position(positionx, positiony, name):
+
+    positionx = str(positionx)
+    positiony = str(positiony)
+    name = str(name)
+    
     path = "dataset/information_data/current/position.py"
     with open(path, "a") as file:
-        file.write(str(position) + ";" + str(name) + "\n")
+        to_write = positionx + "," + positiony + ";" + name + "\n"
+        file.write(to_write)
 
 
 def recup_position(name):
@@ -79,10 +85,11 @@ def recup_position(name):
             increment = ""
             for j in i:
                 if j == ";" or j == "\n":
-                    liste.append(increment[1:])
+                    liste.append(increment)
                     increment = ""
+                else:
+                    increment += j
 
-                increment += j
 
     coordinate = []
     liste_w = []
@@ -99,3 +106,72 @@ def recup_position(name):
         if i != []:
             if i[1] == name:
                 return i[0]
+
+
+
+
+def draw(detection):
+
+    if detection[0] == "": detection[0] = "In searching"
+    name = detection[0]; x = 0; y = 0;
+    increment = "";
+
+    for detec in detection[1]:
+        for d in detec:
+            if d == ",":
+                x = int(increment)
+                increment = ""
+            else:
+                increment += d
+
+    y = int(increment)
+
+
+    path = "dataset/image/current/current.jpg"
+    img = open_picture(path)
+
+    img = cv2.copyMakeBorder(img, 200, 200, 200, 200,
+                             cv2.BORDER_CONSTANT, value=(177, 151, 151))
+
+
+    cv2.circle(img, (x + 200, y + 200), 3, (0,0,255), 3)
+    cv2.line(img, (x + 150, y + 100), (x + 200, y + 200), (255,0,0), 3)
+    cv2.putText(img, name, (x + 100, y + 95), cv2.FONT_HERSHEY_SIMPLEX,
+                1.0, (0, 0, 255), lineType=cv2.LINE_AA) 
+
+    return img
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
