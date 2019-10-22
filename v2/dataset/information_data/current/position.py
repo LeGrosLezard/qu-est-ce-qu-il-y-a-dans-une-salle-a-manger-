6a17566a50799eed0e612d598c4638fc6a35999b
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-10-22 03:41:58 +0200

    Add files via upload

---
+++ v2/dataset/information_data/current/position.py
@@ -0,0 +1,5 @@
+65;dataset/image/current/currentv0.jpg
+82;dataset/image/current/currentv1.jpg
+234;dataset/image/current/currentv2.jpg
+214;dataset/image/current/currentv3.jpg
+145;dataset/image/current/currentv4.jpg
