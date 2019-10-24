6967787e68f19a5a57843968ae812e0a81a3a20f
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-10-24 18:08:22 +0200

    Add files via upload

---
+++ v2/paths.py
@@ -0,0 +1,12 @@
+path_picture_current = "dataset/image/current/current.jpg"
+path_folder_current = "dataset/image/current/"
+
+path_folder_models = "training/models/models/"
+path_label_py = "dataset/information_data/label.py"
+
+path_dataset = "dataset/image/dataset"
+path_folder_folder = "dataset/image/dataset/{}"
+path_image = "dataset/image/dataset/{}/{}"
+
+path_csv_name = "training/csv/in_training/"
+path_model_name = "training/models/in_training/"
