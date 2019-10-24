6a4fb2e9176123207b992c4276b20f9f41aa6a72
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-10-24 16:39:38 +0200

    Update csv_treatment.py

--- v2/training/csv_treatment.py
+++ v2/training/csv_treatment.py
@@ -2,6 +2,64 @@ import os
 import csv
 
 
+def csv_labels(path_current_csv):
+
+    liste_csv = os.listdir(path_current_csv)
+    current_csv = liste_csv[-1]
+
+    print(current_csv)
+
+    path_current_csv = path_current_csv + "/" + str(current_csv)
+
+    file =  open(path_current_csv, 'r')
+    dataframe = file.readlines()
+
+    liste = []
+
+    for i in dataframe:
+        liste.append(i[0])
+
+
+    liste = list(set(liste))
+    liste.remove("l")
+
+    new_name_csv = int(current_csv[:-4]) + 1
+
+    return liste, new_name_csv
+
+
+
+def define_new_csv(liste, path, new_name):
+
+    
+    if len(liste) == 10:
+        
+        csv_create = path + "/" + str(new_name) + ".csv"
+
+        with open(csv_create, "a") as file:
+            pass
+
+        
+
+
+
+
+##
+##path_csv = r"C:\Users\jeanbaptiste\Desktop\assiette\v2\training\csv\csv"
+##
+##liste, new_name_csv = csv_labels(path_csv)
+##define_new_csv(liste, path_csv, new_name_csv)
+
+
+
+
+
+
+
+
+
+
+
 def csv_treatement(path_csv_file, path2):
 
     file =  open(path_csv_file, 'r')
@@ -51,11 +109,12 @@ def csv_treatement(path_csv_file, path2):
 
 
 
-path_csv_file = r"C:\Users\jeanbaptiste\Desktop\assiette\v2\training\csv\in_training\{}"
-path1 = path_csv_file.format("bol.csv")
-path2 = path_csv_file.format("test.csv")
-
-csv_treatement(path1, path2)
+##path_csv_file = r"C:\Users\jeanbaptiste\Desktop\assiette\v2\training\csv\in_training\{}"
+##path1 = path_csv_file.format("Paille.csv")
+##path2 = path_csv_file.format("test.csv")
+##
+##csv_labels(path1)
+#csv_treatement(path1, path2)
 
 
 
