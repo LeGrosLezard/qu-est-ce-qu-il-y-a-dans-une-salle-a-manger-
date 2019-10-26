761d46717e6cdb182a88bc431dfa663c882a7b8c
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-10-26 03:03:52 +0200

    Update step_height.py

--- v2/step_height.py
+++ v2/step_height.py
@@ -18,6 +18,41 @@ def step_height(liste):
     path_model = "../training/models/models"
     liste_model = os.listdir(path_model)
 
+
+    liste_w = []
+    for i in liste_csv:
+        if i == "test.csv":pass
+        else:
+            liste_w.append(len(i))
+
+    maxi = max(liste_w)
+
+
+    liste_w2 = []
+    for i in liste_csv:
+        if len(i) == maxi:
+            liste_w2.append(i)
+
+    labeled = []
+    to_read = path_csv + "/" + str(max(liste_w2))
+    f =  open(to_read, 'r')
+    dataframe = f.readlines()
+    dataframe = dataframe
+    for i in dataframe:
+        try:
+            labeled.append(int(i[0]))
+        except:
+            pass
+
+    labeled = max(list(set(labeled)))
+    print(labeled)
+
+    label = labeled + 1
+
+
+
+
+
     to_change = []
     for i in name:
         for csv in liste_training_csv:
@@ -37,23 +72,23 @@ def step_height(liste):
 
 
         main = open("../training/csv/csv/test.csv", 'w')
-        
+
         for j in dataframe:
             ok = False
             if j[0] == "1":
                 j = j.split(";")
-                j[0] = "5"
+                j[0] = str(label)
 
                 for k in j[:-1]:
                     main.write(str(k)+";")
-                
+
                 ok = True
 
             if ok is True:
                 main.write("\n")
 
 
-
+        label += 1
 
 
 
@@ -66,3 +101,17 @@ def step_height(liste):
 
 liste =  [['Fourchette', 20, 100, '../dataset/image/current/currentv0.jpg'], ['Fourchette', 20, 100, '../dataset/image/current/currentv00.jpg'], ['Cuillere', 20, 100, '../dataset/image/current/currentv000.jpg'], ['Cuillere', 20, 100, '../dataset/image/current/currentv1.jpg'], ['Fourchette', 20, 100, '../dataset/image/current/currentv1.jpg'], ['Cuillere', 20, 100, '../dataset/image/current/currentv3.jpg']]
 step_height(liste)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
